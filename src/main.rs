a5e3a1d652fd48aa045b5f2fa7bc3301b8441f90
Gao <1148118271@qq.com>
2022-04-26 15:22:29 +0800

    t

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn c() {
     let u = stream.read(&mut s).unwrap();
     println!("{:?}", String::from_utf8((&s[..u]).to_vec()).unwrap());
     let mut string = String::new();
-    for i in 0..101600 {
+    for i in 0..1016 {
         string = string.add("s");
     }
     let string1 = format!("set key {}\r\n", string);

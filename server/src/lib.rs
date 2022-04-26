38dadfeb6b3e8ecc1ead61aaddbe7818c2c37501
gaoxiangkang <gxk_717@163.com>
2022-04-26 15:17:30 +0800

    tcp non-blocking

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,7 +1,6 @@
 use tokio::io;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
-use tokio::net::tcp::{ReadHalf, WriteHalf};
 use parse::Type;
 
 
@@ -18,93 +17,53 @@ impl Server {
         println!("server started.");
         loop {
             let (mut socket, addr) = self.0.accept().await?;
-            let (mut r, mut w) = socket.into_split();
             println!("[{}] client connection.", addr);
             tokio::spawn(async move {
-
-                w.write(b"200 HELLO.\r\n").await.unwrap();
-                // let mut buf = [0; 1024];
-                // let i = r.read(&mut buf).await.unwrap();
-                // let text = unsafe {
-                //     String::from_utf8_unchecked((&buf[..i]).to_vec())
-                // };
-                // println!("{}", text);
-                // let t = parse::Type::get_type(&text);
-                // match t {
-                //     Type::Set => {
-                //         println!("set command.");
-                //         w.write(b"1 success.\r\n").await.unwrap();
-                //     }
-                //     Type::Get => {},
-                //     Type::Null => {}
-                // }
-                // w.write(b"200 HELLO.\r\n").await.unwrap();
-                // if let Err(e) = Server::write(&mut w, b"200 HELLO.\r\n").await {
-                //     eprintln!("write error, error info: {}", e);
-                //     return;
-                // }
-                // let text = match Server::read_string(&mut r).await {
-                //     Ok(text) => text,
-                //     Err(e) => {
-                //         eprintln!("read string error, error info: {}", e);
-                //         return;
-                //     }
-                // };
-                // w.write(b"1 success.\r\n").await.unwrap();
-                // Server::handle(&mut w, &text).await.unwrap();
-                // w.write(b"1 success.\r\n").await.unwrap();
-                // println!("[{}] disconnection connection.", addr);
+                if let Err(e) = Server::write(&mut socket, b"200 HELLO.\r\n").await {
+                    eprintln!("write error, error info: {}", e);
+                    return;
+                }
+                let text = match Server::read_string(&mut socket).await {
+                    Ok(text) => text,
+                    Err(e) => {
+                        eprintln!("read string error, error info: {}", e);
+                        return;
+                    }
+                };
+                Server::handle(&mut socket, &text).await.unwrap();
+                println!("[{}] disconnection connection.", addr);
             });
         }
     }
 
 
-    async fn handle(socket: &mut WriteHalf<'_>, text: &str) -> Result<(), ()> {
+    async fn handle(s: &mut TcpStream, text: &str) -> io::Result<()> {
         let t = parse::Type::get_type(&text);
         match t {
             Type::Set => {
                 println!("set command.");
+                Server::write(s, b"0 SUCCESS.").await?
             }
             Type::Get => {}
-            Type::Null => {return Err(())}
+            Type::Null => Server::write(s, b"1 FAIL.").await?
         }
         Ok(())
     }
 
-    async fn write(w: &mut WriteHalf<'_>, buf: &[u8]) -> io::Result<()> {
-        loop {
-            match w.write(buf).await {
-                Ok(_) => break,
-                Err(e) => {
-                    if e.kind() == io::ErrorKind::WouldBlock {
-                        continue;
-                    }
-                    return Err(e);
-                }
-            }
-        }
+    async fn write(s: &mut TcpStream, buf: &[u8]) -> io::Result<()> {
+        s.writable().await?;
+        s.try_write(buf)?;
         Ok(())
     }
 
-    async fn read_string(r: &mut ReadHalf<'_>) -> io::Result<String> {
-        // socket.readable().await?;
+    async fn read_string(s: &mut TcpStream) -> io::Result<String> {
         let mut buf = [0; 1024];
         let mut bufs = vec![];
         loop {
-            match r.read(&mut buf).await {
-                Ok(n) => {
-                    if n == 0 {
-                        break;
-                    }
-                    bufs.extend_from_slice(&buf[..n]);
-                    continue;
-                },
-                Err(e) => {
-                    if e.kind() == io::ErrorKind::WouldBlock {
-                        continue;
-                    }
-                    return Err(e);
-                }
+            s.readable().await?;
+            match s.try_read(&mut buf) {
+                Ok(n) =>  bufs.extend_from_slice(&buf[..n]),
+                Err(e) => break
             }
         }
         let text = unsafe {
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::io;
 use std::io::{Read, Write};
 use std::net::TcpStream;
+use std::ops::Add;
 
 #[tokio::main]
 async fn main() {
@@ -13,7 +15,12 @@ fn c() {
     let mut s = [0; 128];
     let u = stream.read(&mut s).unwrap();
     println!("{:?}", String::from_utf8((&s[..u]).to_vec()).unwrap());
-    stream.write_all(b"set key sss");
+    let mut string = String::new();
+    for i in 0..101600 {
+        string = string.add("s");
+    }
+    let string1 = format!("set key {}\r\n", string);
+    stream.write_all(string1.as_bytes());
     stream.flush();
     let u = stream.read(&mut s).unwrap();
     println!("{:?}", String::from_utf8((&s[..u]).to_vec()).unwrap());

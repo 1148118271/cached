797d81eed38f16365152fad4eee183691293258f
Gao Xiangkang <1148118271@qq.com>
2022-05-01 20:28:43 +0800

    删除无用代码

--- src/main.rs
+++ src/main.rs
@@ -5,36 +5,3 @@ async fn main() {
     let server = server::Server::new().await.unwrap();
     server.run().await.unwrap();
 }
-
-
-// use std::io::{Read, Write};
-// use std::net::{Shutdown, TcpStream};
-//
-// #[test]
-// fn c() {
-//     let mut stream = TcpStream::connect("127.0.0.1:9200").unwrap();
-//     let mut s = [0; 128];
-//     let u = stream.read(&mut s).unwrap();
-//     println!("{:?}", String::from_utf8((&s[..u]).to_vec()).unwrap());
-//     stream.write_all(b"set test gaoxiangkang");
-//     stream.flush();
-//     let u = stream.read(&mut s).unwrap();
-//     println!("{:?}", String::from_utf8((&s[..u]).to_vec()).unwrap());
-//     stream.write_all(b"get test").unwrap();
-//     stream.flush();
-//     let u = stream.read(&mut s).unwrap();
-//     println!("{:?}", String::from_utf8((&s[..u]).to_vec()).unwrap());
-//
-//     stream.write_all(b"rm test").unwrap();
-//     stream.flush();
-//     let u = stream.read(&mut s).unwrap();
-//     println!("{:?}", String::from_utf8((&s[..u]).to_vec()).unwrap());
-//
-//
-//     stream.write_all(b"get test").unwrap();
-//     stream.flush();
-//     let u = stream.read(&mut s).unwrap();
-//     println!("{:?}", String::from_utf8((&s[..u]).to_vec()).unwrap());
-//
-//     stream.shutdown(Shutdown::Both).expect("shutdown call failed");
-// }
